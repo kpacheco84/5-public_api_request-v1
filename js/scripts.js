8f0a566965547178358d3ba976121414db086032
kpacheco84 <45826075+kpacheco84@users.noreply.github.com>
2019-03-25 12:02:03 -0400

    almost ready
    everything working except data not showing for module

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,7 @@
-//Get and display 12 random users
-/*With information provided from The Random User Generator API, send a request to the API, 
-and use the response data to display 12 users, along with some basic information for each:*/
-// fetch function
-/* items first name
-console.log(item.name.first)    
-     var nameHTML = $('#name').html();
-     console.log(nameHTML)
-     //$(nameHTML).empty().html(item.name.first);
-*/
 
-
-/*for reference of the card html
-     <div class="card">
-                    <div class="card-img-container">
-                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-                    </div>
-                    <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">first last</h3>
-                        <p class="card-text">email</p>
-                        <p class="card-text cap">city, state</p>
-                    </div>
-                </div>
-                       
-        </div>
-
-*/
 
  function getResults(results){ 
-  //hide initial card ?
+ 
  
   
   //add each rsult to card
@@ -47,7 +21,7 @@ console.log(item.name.first)
 
     
       
-    Cardhtml     =    `<div class="card"><div id = "card-${[i]}" class="card-img-container">
+    Cardhtml     =    `<div id = "card-${[i]}" class="card"><div class="card-img-container">
                       <img class="card-img" src="${pictureLrg}" alt="profile picture"></div>       
                       <div class="card-info-container">
                       <h3 id="name" class="card-name cap">${name}</h3>
@@ -55,13 +29,12 @@ console.log(item.name.first)
                       </div>
                       </div>
                       </div> `  
-    
-    //$(cardGallery).append($("<div>",{'class':'card'}))
-      //example chaining in jquery 
-      //$("body").html($("<select>").append($("<option>", { 'value': 'bla bla bla' })).append
-    
+ 
       // add card to gallery
       $(cardGallery).append(Cardhtml);
+
+
+
       
      const modalContainerhtml = `<div id = "cardContainer-${[i]}" class="modal-container">
                                 <div class="modal"></div>
@@ -77,38 +50,41 @@ console.log(item.name.first)
                                 </div>
                                 </div>`
     
-    // dont know if i need this
- //$(cardGallery).append(modalContainerhtml);  
-  
-   $(".cardContainer-"+[i]).addClass('hidden')
  
-   console.log(modalContainerhtml)
+ $("body").append(modalContainerhtml);  
+  
+  $("#cardContainer-"+[i]).hide();
+ 
+ 
 
 
-
-
-
-
-//console.log(Cardhtml)
 
 }//end of for each
+ )
+ //click event for card to open module
+  $(".card").on("click", function(){
+   
+   const clickedCard =($(this).attr("id").replace(/card-/, ''));
+   const module =  $(".modal-container").attr("id").replace(/cardContainer-/, '');
 
+  $("#cardContainer-"+ clickedCard).show();
 
-     
-  )
-  // event listener for card
-$(".card-img-container").on("click", function(){
-  console.log('card has been clicked')
- // $("#modal-container").addClass("active");
-  });
+});
 
+// click event to close module 
+
+$(".modal-close-btn").on("click", function(){
+   
+ $(".modal-container").hide();
+
+});
 
 }
-  ;
+  
 
 
 //fetch statement
-    fetch('https://randomuser.me/api/?results=12')
+    fetch('https://randomuser.me/api/?nat=us&results=12')
     .then(res => res.json())
     
     .then(out => out.results)
@@ -116,77 +92,54 @@ $(".card-img-container").on("click", function(){
     .then((results) =>  getResults(results));
 //end of fetch
       
-// event listener for card
-    $("#card-img-container").on("click", function(){
-      console.log('card has been clicked')
-     // $("#modal-container").addClass("active");
-      });
 
 
-        
-//}).catch(err => console.error(err));
+// APPEND SEARCH FUNCTIONALITY
+/*
+
+function btnSearch(){
+const btnSearchhtml = `<form action="#" method="get">
+                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                        </form>`;
+
+// append btn html to search container
+$(".search-container").append(btnSearchhtml);
+$('form').show();   
+
+}   ;
+
+
+
+btnSearch();
+
+*/
+
+
 
 
 
 
  
-//Image
-//First and Last Name
-//Email
-//City or location
-//Refer to the mockups and the comments in the index.html file 
-//for an example of what info 
-//should be displayed on the page and how it should be styled.
 
-
-/*Create a modal window
-When any part of an employee item in the directory is clicked, a modal window 
-should pop up with the following details displayed:*/
-
-//Image
-//Name
-//Email
-//City or location
-//Cell Number
-//Detailed Address, including street name and number, state or country, and post code.
-//Birthday
-//Make sure there’s a way to close the modal window
+//EXCEEDS
+//}).catch(err => console.error(err));
 /*
-Modal markup:
+Search
+Add a way to filter the directory by name. To do this, you’ll need to adjust your API request to retrieve a user nationality that will only return data in the English alphabet.
+Example markup for this feature is included in the HTML comments.
+Note: Your search feature should filters results that are already on the page. So don't request new info from the API for your search.
 
-You can use the commented out markup below as a template
-for your modal, but you must use JS to create and append 
-it to `body`.
-
-IMPORTANT: Altering the arrangememnt of the markup and the 
-attributes used may break the styles or functionality.
-
-<div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>
-    </div>
-
-
-    example pop up functionality
-    $(".open").on("click", function(){
-$(".popup, .popup-content").addClass("active");
-});
-When this happens, the “active” class is appended to the pop-up classes, thus changing the pop-up’s visibility to “visible.” In other words, the pop-up will appear.
-
-This second statement removes the “active” class when the “Close” button is clicked.
-
-$(".close, .popup").on("click", function(){
-$(".popup, .popup-content").removeClass("active");
-});
+Modal toggle
+Add a way to toggle back and forth between employees when the modal window is open.
+There should be no errors once the end or beginning of the list is reached.
+Example markup for this feature is included in the HTML comments.
+Structure, style and CSS
+Add or change at least one of the following:
+color
+background color
+font
+box or text shadows
+Document your style changes in your readme file and the project submission notes.
+Do not alter the layout or position of the important elements on the page.
 */
-
